60dccded0c05376aec877ef0573f6305902e3b0d
luan.mai-mesoneer <luan.mai@mesoneer.io>
2022-07-30 16:26:16 +0700

    add color & debug for box

--- box.sh
+++ box.sh
@@ -1,16 +1,27 @@
+# color
+water=123
+purple=125
+blue=$(tput setaf 4)
+
+
 function box()
 {
   local s=("$@") b w
   for l in "${s[@]}"; do
     ((w<${#l})) && { b="$l"; w="${#l}"; }
   done
-  tput setaf 3
+  tput setaf $purple
   echo " -${b//?/-}-
 | ${b//?/ } |"
   for l in "${s[@]}"; do
-    printf '| %s%*s%s |\n' "$(tput setaf 4)" "-$w" "$l" "$(tput setaf 3)"
+    printf '| %s%*s%s |\n' "$(tput setaf $water)" "-$w" "$l" "$(tput setaf $purple)"
   done
   echo "| ${b//?/ } |
  -${b//?/-}-"
   tput sgr 0
 }
+
+# debug
+box "Package name: content" \
+    "Next package version: content" \
+    "Current package version: content"
