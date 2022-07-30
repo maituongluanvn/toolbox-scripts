96b722e63d8619e52284f50490f4514cf34d091f
luan.mai-mesoneer <luan.mai@mesoneer.io>
2022-07-30 14:55:33 +0700

    2.4.0

--- npm-preversion.sh
+++ npm-preversion.sh
@@ -9,16 +9,16 @@ packageVersion=$(cat package.json \
 
 minorVersion=$(echo "$packageVersion" | cut -d "." -f 2)
 
-echo "packageVersion:" $packageVersion
-echo "minorVersion:" $minorVersion
+echo "********** Current package version:" $packageVersion
+echo "********** minorVersion:" $minorVersion
 
 # git tag $packageVersion
 # git push origin $packageVersion
 
 # get latest source code from master
-# git reset --hard origin/master
+git reset --hard origin/master
 
 # bump version
 # nextMinorVersion=$(npm version minor)
-nextMinorVersion=$(echo $(npm version minor) | cut -d "v" -f 1)
-echo $nextMinorVersion
+nextVersion=$(echo $(npm version minor) | cut -d "v" -f 1)
+echo "********** Next version" $nextVersion
