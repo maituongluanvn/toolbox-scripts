198dbb435077918970bdfec128928eee417cb9ad
luan.mai-mesoneer <luan.mai@mesoneer.io>
2022-08-03 10:44:12 +0700

    github action

---
+++ .github/.ci/npm-preversion.sh
@@ -0,0 +1,34 @@
+#!/bin/sh
+set -e
+source ../../example/table.sh
+# get latest source code from master
+git reset --hard origin/master
+
+packageVersion=$(cat ../package.json \
+                | grep version \
+                | head -1 \
+                | awk -F: '{ print $2 }' \
+                | sed 's/[",]//g')
+
+# tag package
+git tag $packageVersion
+git push origin $packageVersion
+
+# bump version
+nextMinorVersion=$(echo $(npm version minor) | cut -d "v" -f 2)
+# delete local redundant tag
+git tag -d "v$nextMinorVersion"
+
+git push
+
+# echo "aaaaaaaaaa:" $nextMinorVersion
+# echo "packageVersion:" $packageVersion
+packageName=$(npm run env | grep "npm_package_name" | awk -F "=" '{print $2}')
+
+# box "Package name: $packageName" \
+#     "Next package version: $nextMinorVersion" \
+#     "Current package version: $packageVersion"
+
+printTable ',' \
+"Package name , Current package version , Next package version
+$packageName , $packageVersion , $nextMinorVersion"
